6ce09245bbe18b884027694f005119222139d61c
Shubhamjindal0801 <52204183+Shubhamjindal0801@users.noreply.github.com>
2023-04-26 00:31:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const byteSize = (str) => {
-  // write your code here
+  const len = str.length;
+	return len;
 };
 
 // Do not change the code below
+
 const str = prompt("Enter some string.");
 alert(byteSize(str));
